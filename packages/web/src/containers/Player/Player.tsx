3915fbf07caa0543eb17bbb695f61e2b5475df84
Marcin Makarewicz <marcin@makarewicz.eu>
2021-09-01 20:53:44 +0200

    feat: add waveform

--- packages/web/src/containers/Player/Player.tsx
+++ packages/web/src/containers/Player/Player.tsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
 import useSound from "use-sound";
+import cx from "classnames";
+import { Waveform } from "./Waveform";
 import styles from "./styles.module.scss";
+
+const track = require("./assets/running_out.webm").default;
 
 export function Player() {
   const [isPlaying, setIsPlaying] = useState(false);
 
-  const [play, { sound, stop }] = useSound(
-    require("./assets/running_out.webm").default
-  );
+  const [play, { sound, stop }] = useSound(track);
 
   useEffect(() => {
     if (sound) {
       sound.on("play", () => setIsPlaying(true));
       sound.on("stop", () => setIsPlaying(false));
+      sound.on("end", () => setIsPlaying(false));
       const interval = setInterval(
         () => console.log({ seek: sound.seek() }),
         1000
@@ -21,6 +24,7 @@ export function Player() {
         clearInterval(interval);
         sound.off("play");
         sound.off("stop");
+        sound.off("end");
         sound.unload();
       };
     }
@@ -29,11 +33,24 @@ export function Player() {
   return (
     <div className={styles.Player}>
       Player{" "}
-      {isPlaying ? (
-        <button onClick={(e) => stop()}>Stop</button>
-      ) : (
-        <button onClick={(e) => play()}>Play</button>
-      )}
+      <div className={styles.Container}>
+        {isPlaying ? (
+          <button
+            onClick={(e) => stop()}
+            className={cx(styles.Button, styles.Button__Stop)}
+          >
+            Stop
+          </button>
+        ) : (
+          <button
+            onClick={(e) => play()}
+            className={cx(styles.Button, styles.Button__Play)}
+          >
+            Play
+          </button>
+        )}
+        <Waveform src={track} />
+      </div>
     </div>
   );
 }
