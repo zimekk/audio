5455408500e935a8b440e968abf541bd0bdd3fa6
Marcin Makarewicz <marcin@makarewicz.eu>
2022-04-18 22:19:59 +0200

    feat(web): piano sampler

--- packages/web/src/containers/Piano/index.tsx
+++ packages/web/src/containers/Piano/index.tsx
@@ -1,10 +1,12 @@
 import React, {
   UIEventHandler,
+  ChangeEventHandler,
   useCallback,
   useEffect,
   useMemo,
   useRef,
   useState,
+  KeyboardEventHandler,
 } from "react";
 import * as Tone from "tone";
 import { Midi } from "@tonejs/midi";
@@ -51,27 +53,67 @@ const keys = Object.entries({
 
 // https://github.com/Tonejs/Tone.js/blob/dev/examples/monoSynth.html
 const usePiano = () => {
-  const [synth] = useState(() =>
-    new Tone.PolySynth(Tone.MonoSynth, {
-      volume: -8,
-      oscillator: {
-        type: "square8",
-      },
-      envelope: {
-        attack: 0.05,
-        decay: 0.3,
-        sustain: 0.4,
-        release: 0.8,
-      },
-      filterEnvelope: {
-        attack: 0.001,
-        decay: 0.7,
-        sustain: 0.1,
-        release: 0.8,
-        baseFrequency: 300,
-        octaves: 4,
-      },
-    }).toDestination()
+  const [synth] = useState(
+    () =>
+      new Tone.Sampler({
+        urls: {
+          A0: "A0.mp3",
+          C1: "C1.mp3",
+          "D#1": "Ds1.mp3",
+          "F#1": "Fs1.mp3",
+          A1: "A1.mp3",
+          C2: "C2.mp3",
+          "D#2": "Ds2.mp3",
+          "F#2": "Fs2.mp3",
+          A2: "A2.mp3",
+          C3: "C3.mp3",
+          "D#3": "Ds3.mp3",
+          "F#3": "Fs3.mp3",
+          A3: "A3.mp3",
+          C4: "C4.mp3",
+          "D#4": "Ds4.mp3",
+          "F#4": "Fs4.mp3",
+          A4: "A4.mp3",
+          C5: "C5.mp3",
+          "D#5": "Ds5.mp3",
+          "F#5": "Fs5.mp3",
+          A5: "A5.mp3",
+          C6: "C6.mp3",
+          "D#6": "Ds6.mp3",
+          "F#6": "Fs6.mp3",
+          A6: "A6.mp3",
+          C7: "C7.mp3",
+          "D#7": "Ds7.mp3",
+          "F#7": "Fs7.mp3",
+          A7: "A7.mp3",
+          C8: "C8.mp3",
+        },
+        release: 1,
+        baseUrl: "https://tonejs.github.io/audio/salamander/",
+        onload: () => {
+          // synth.triggerAttackRelease(["C1", "E1", "G1", "B1"], 0.5);
+        },
+      }).toDestination()
+    // new Tone.PolySynth(Tone.MonoSynth, {
+    //   volume: -8,
+    //   oscillator: {
+    //     type: "square8",
+    //   },
+    //   envelope: {
+    //     attack: 0.05,
+    //     decay: 0.3,
+    //     sustain: 0.4,
+    //     release: 0.8,
+    //   },
+    //   filterEnvelope: {
+    //     attack: 0.001,
+    //     decay: 0.7,
+    //     sustain: 0.1,
+    //     release: 0.8,
+    //     baseFrequency: 300,
+    //     octaves: 4,
+    //   },
+    // }).toDestination()
   );
 
   return synth;
@@ -350,9 +392,9 @@ const useKeyboard = (synth) => {
   );
 
   useEffect(() => {
-    const handleKeyDown = (e) =>
+    const handleKeyDown: KeyboardEventHandler = (e) =>
       notes[e.code] && synth.triggerAttack(notes[e.code]);
-    const handleKeyUp = (e) =>
+    const handleKeyUp: KeyboardEventHandler = (e) =>
       notes[e.code] && synth.triggerRelease(notes[e.code]);
     document.addEventListener("keydown", handleKeyDown);
     document.addEventListener("keyup", handleKeyUp);
